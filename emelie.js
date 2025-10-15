9ebb8c9fd4bc66c0f038434060a3b847870863c2
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2025-10-15 16:06:09 +0200

    added window.open

--- emelie.js
+++ emelie.js
@@ -1,6 +1,6 @@
 const emelieImg = document.getElementById("emelie");
 
-
 emelieImg.addEventListener("dblclick", () => {
   alert("GitHub: https://github.com/bjorkmanemelie");
-}); 
+  window.open("https://github.com/bjorkmanemelie");
+});
