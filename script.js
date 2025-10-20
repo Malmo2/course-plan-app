d51d841b8c709b16511abc3f451179d1e6e6a922
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-10-20 11:40:33 +0200

    djoidij

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const listItems = document.querySelectorAll('li');
+const listItems = document.querySelectorAll('.knowledge-list li');
+const buttons = document.querySelectorAll('.dropdown-btn');
 
 listItems.forEach(li => {
-    li.querySelector('.li-header').addEventListener('click', () => {
-        li.classList.toggle('active')
-    });
+  li.querySelector('.li-header').addEventListener('click', () => {
+    li.classList.toggle('active');
+  });
+});
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.stopPropagation(); 
+    const li = btn.closest('li');
+    li.classList.toggle('active');
+  });
 });
