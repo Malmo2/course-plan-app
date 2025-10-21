377718a2fb2d5ab4d7f9dde9daa0bfa524d4253e
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-10-21 08:50:39 +0200

    fungerande dropdown

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const listItems = document.querySelectorAll('.knowledge-list li');
 const buttons = document.querySelectorAll('.dropdown-btn');
-
-listItems.forEach(li => {
-  li.querySelector('.li-header').addEventListener('click', () => {
-    li.classList.toggle('active');
-  });
-});
+const closeButtons = document.querySelectorAll('.close-btn');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', (e) => {
     e.stopPropagation(); 
     const li = btn.closest('li');
     li.classList.toggle('active');
+  });
+});
+
+closeButtons.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+e.stopPropagation();
+const li = btn.closest('li');
+li.classList.toggle('active');
+li.classList.remove();
   });
 });
